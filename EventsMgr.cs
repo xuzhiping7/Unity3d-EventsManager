986a9c7c58335b205fee5299aa8ad991a848b0be
xuzhiping7 <xuzhiping7@qq.com>
2014-07-24 18:25:09 +0800

    upload scripts

---
+++ EventsMgr.cs
@@ -0,0 +1,112 @@
+﻿//*******************************************
+//文件名称: 简单的全局事件管理
+//文件描述:	简单的全局事件管理系统，主要用于事件触发（新手引导）
+//参与编写人: 徐志平
+//*******************************************
+
+
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public enum eEventsKey
+{
+    控制系统_兔子跳,
+    控制系统_兔子向左走,
+    控制系统_兔子停止向左走,
+    控制系统_兔子向右走,
+    控制系统_兔子停止向右走,
+}
+
+
+public class EventsMgr  {
+
+    private static EventsMgr instance;
+    public static EventsMgr GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new EventsMgr();
+        }
+        return instance;
+    }
+
+
+    //公共委托事件
+    public delegate void CommonEvent(object data);
+
+    //事件列表
+    public Dictionary<eEventsKey, CommonEvent> m_dicEvents = new Dictionary<eEventsKey, CommonEvent>();
+
+    
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    public void Init()
+    {
+        foreach (eEventsKey key in eEventsKey.GetValues(typeof(eEventsKey)))
+        {
+            AddDelegate(key);
+        }
+    }
+
+
+    /// <summary>
+    ///  添加一个回调事件
+    /// </summary>
+    private void AddDelegate(eEventsKey key)
+    {
+        //Debug.Log(" 添加一个回调 : "+ key);
+        m_dicEvents.Add(key, delegate(object data) { });
+    }
+
+
+    /// <summary>
+    /// 事件触发
+    /// </summary>
+    public void TriigerEvent(eEventsKey key, object param)
+    {
+        if (m_dicEvents.ContainsKey(key))
+        {
+            //Debug.Log(" 事件回调 : " + key);
+            m_dicEvents[key](param);
+        }
+        else
+        {
+            Debug.LogError("没有这个定义的事件:" + key);
+        }
+    }
+
+    /// <summary>
+    /// 事件绑定
+    /// </summary>
+    public void AttachEvent(eEventsKey strEventKey, CommonEvent attachEvent)
+    {
+        if (m_dicEvents.ContainsKey(strEventKey))
+        {
+            //Debug.Log(" 事件绑定 : " + strEventKey);
+            m_dicEvents[strEventKey] += attachEvent;
+        }
+        else
+        {
+            Debug.LogError("没有这个定义的事件:" + strEventKey);
+        }
+    }
+
+
+    /// <summary>
+    /// 去除事件绑定
+    /// </summary>
+    public void DetachEvent(eEventsKey strEventKey, CommonEvent attachEvent)
+    {
+        if (m_dicEvents.ContainsKey(strEventKey))
+        {
+            //Debug.Log("去除事件绑定 : " + strEventKey);
+            m_dicEvents[strEventKey] -= attachEvent;
+        }
+        else
+        {
+            Debug.LogError("没有这个定义的事件:" + strEventKey);
+        }
+    }
+}
---
+++ testEvent.cs
@@ -0,0 +1,62 @@
+﻿//*******************************************
+//文件名称: 测试全局事件管理系统
+//文件描述:	测试步骤  初始化，绑定事件，触发事件，消除事件。
+//参与编写人: 徐志平
+//*******************************************
+
+
+using UnityEngine;
+using System.Collections;
+
+public class testEvent : MonoBehaviour {
+
+
+	void Start () {
+
+        EventsMgr.GetInstance().Init();
+
+        attachEvents();
+
+        TriggerEvents();
+
+        detachrEvents();
+	}
+
+    void attachEvents()
+    {
+        EventsMgr.GetInstance().AttachEvent(eEventsKey.控制系统_兔子跳, RabbitJump);
+        EventsMgr.GetInstance().AttachEvent(eEventsKey.控制系统_兔子向右走, RabbitRight);
+        EventsMgr.GetInstance().AttachEvent(eEventsKey.控制系统_兔子停止向右走, RabbitStopRight);
+    }
+
+    void TriggerEvents()
+    {
+        EventsMgr.GetInstance().TriigerEvent(eEventsKey.控制系统_兔子跳, 10.0f);
+        EventsMgr.GetInstance().TriigerEvent(eEventsKey.控制系统_兔子向右走, 5.0f);
+        EventsMgr.GetInstance().TriigerEvent(eEventsKey.控制系统_兔子停止向右走, 5.0f);
+    }
+
+    void detachrEvents()
+    {
+        EventsMgr.GetInstance().DetachEvent(eEventsKey.控制系统_兔子跳, RabbitJump);
+        EventsMgr.GetInstance().DetachEvent(eEventsKey.控制系统_兔子向右走, RabbitRight);
+        EventsMgr.GetInstance().DetachEvent(eEventsKey.控制系统_兔子停止向右走, RabbitStopRight);
+    }
+
+
+    void RabbitJump(object data)
+    {
+        Debug.Log("RabbitJump JumpHeight:" + (float)data);
+    }
+
+    void RabbitRight(object data)
+    {
+        Debug.Log("RabbitRight Speed:" + (float)data);
+    }
+
+    void RabbitStopRight(object data)
+    {
+        Debug.Log("RabbitStop");
+    }
+
+}
